00acd1ee2a9aa657c91818ef2540a6593126cf2a
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-02-11 20:10:44 -0800

    Improve code structure.

--- prog1.cpp
+++ prog1.cpp
@@ -1,21 +1,33 @@
 #include <list>
 
-void insert(int num, std::list <int> ** table){
-	int hash_value = num%43;
+const int table_length = 43;
+
+int calculate_hash_value(int num){
+	int hash_value = num % table_length;
 	if (hash_value < 0){
-		hash_value = hash_value + 43;
+		hash_value = hash_value + table_length;
 	}
+	return hash_value;
+}
+
+void insert(int num, std::list <int> ** table){
+	int hash_value = calculate_hash_value(num);
 	table[hash_value]->push_front(num);
 }
 
+void find(int num, std::list <int> ** table){
+
+}
+
 int main(int argc, char** argv){
-std::list <int> * hash_table [43];
+std::list <int> * hash_table [table_length];
 std::list <int> ** hash_table_pointer;
 hash_table_pointer = hash_table;
-for (int i = 0; i < 43; ++i){
+for (int i = 0; i < table_length; ++i){
 	std::list <int> my_list;
 	std::list <int> * my_list_pointer = &my_list;
 	hash_table[i] = my_list_pointer;
 }
+insert(300, hash_table_pointer);
 return 0;
 }
