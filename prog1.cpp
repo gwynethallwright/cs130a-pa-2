e980d17cc23c99474fae4599b06f054c46ae0895
Gwyneth Allwright <gwyneth@ucsb.edu>
2020-02-12 20:34:29 -0800

    Add option to suppress cout from find.

--- prog1.cpp
+++ prog1.cpp
@@ -14,22 +14,26 @@ int calculate_hash_value(int num){
 	return hash_value;
 }
 
-auto find(int num, std::vector<std::array<int, 2>> ** table){
+auto find(int num, std::vector<std::array<int, 2>> ** table, int suppress_output){
 	struct result {int return_value; std::array<int, 2> * pointer;};
 	int hash_value = calculate_hash_value(num);
 	std::vector<std::array<int, 2>> :: iterator it;
 	for (it = table[hash_value]->begin(); it != table[hash_value]->end(); ++it){
 		if ((*it)[0] == num){
-			std::cout << "Found.\n";
+			if (suppress_output == 0){
+				std::cout << "Found.\n";
+			}
 			return result {1, &(*it)};
 		}
 	}
-	std::cout << "Not found.\n";
+	if (suppress_output == 0){
+		std::cout << "Not found.\n";
+	}
 	return result {0, &(*it)};
 }
 
 void insert(int num, std::vector<std::array<int, 2>> ** table){
-	auto [found, pointer] = find(num, table);
+	auto [found, pointer] = find(num, table, 1);
 	if (found == 0){
 		int hash_value = calculate_hash_value(num);
 		static std::array<int, 2> my_array;
@@ -61,7 +65,7 @@ int main(int argc, char** argv){
 	std::vector<std::array<int, 2>> ** hash_table_pointer = create_hash_table();
 	insert(300, hash_table_pointer);
 	insert(300+table_length, hash_table_pointer);
-	find(300, hash_table_pointer);
-	find(300+table_length, hash_table_pointer);
+	find(300, hash_table_pointer, 0);
+	find(300+table_length, hash_table_pointer, 0);
 	return 0;
 }
